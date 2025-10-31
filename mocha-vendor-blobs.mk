691a041cba250a382ba4893ad59b535d9065dd02
apple <105837597+380583067@users.noreply.github.com>
2025-10-31 14:19:55 +0000

    Fix CPU heat automatic control, stable heating

--- mocha-vendor-blobs.mk
+++ mocha-vendor-blobs.mk
@@ -75,6 +75,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/mocha/proprietary/lib/libsensors.lsm6db0.so:system/lib/libsensors.lsm6db0.so \
     vendor/xiaomi/mocha/proprietary/lib/libsensors.mpu6515.so:system/lib/libsensors.mpu6515.so \
     vendor/xiaomi/mocha/proprietary/lib/hw/ts.default.so:system/lib/hw/ts.default.so \
+    vendor/xiaomi/mocha/proprietary/vendor/lib/hw/thermal.tegra.so:system/vendor/lib/hw/thermal.tegra.so \
     vendor/xiaomi/mocha/proprietary/lib/libFaceProc.so:system/lib/libFaceProc.so \
     vendor/xiaomi/mocha/proprietary/lib/libbeautify.so:system/lib/libbeautify.so \
     vendor/xiaomi/mocha/proprietary/vendor/lib/libbt-client-api.so:system/vendor/lib/libbt-client-api.so \
